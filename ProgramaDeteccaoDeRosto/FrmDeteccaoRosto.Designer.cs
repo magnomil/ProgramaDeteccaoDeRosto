26dcf60efb56b221c598c8a37b5b37593a1f43d6
magnomil <67349729+magnomil@users.noreply.github.com>
2021-08-18 23:09:27 -0300

    Adicionar arquivos de projeto.

---
+++ ProgramaDeteccaoDeRosto/FrmDeteccaoRosto.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace ProgramaDeteccaoDeRosto
+{
+    partial class FrmDeteccaoRosto
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.abriImagemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarWebCamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pararWebCamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abriImagemToolStripMenuItem,
+            this.iniciarWebCamToolStripMenuItem,
+            this.pararWebCamToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // picImagem
+            // 
+            this.picImagem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picImagem.Location = new System.Drawing.Point(0, 28);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(800, 422);
+            this.picImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImagem.TabIndex = 1;
+            this.picImagem.TabStop = false;
+            // 
+            // abriImagemToolStripMenuItem
+            // 
+            this.abriImagemToolStripMenuItem.Name = "abriImagemToolStripMenuItem";
+            this.abriImagemToolStripMenuItem.Size = new System.Drawing.Size(115, 24);
+            this.abriImagemToolStripMenuItem.Text = "Abrir Imagem";
+            this.abriImagemToolStripMenuItem.Click += new System.EventHandler(this.abriImagemToolStripMenuItem_Click);
+            // 
+            // iniciarWebCamToolStripMenuItem
+            // 
+            this.iniciarWebCamToolStripMenuItem.Name = "iniciarWebCamToolStripMenuItem";
+            this.iniciarWebCamToolStripMenuItem.Size = new System.Drawing.Size(131, 24);
+            this.iniciarWebCamToolStripMenuItem.Text = "Iniciar Web Cam";
+            this.iniciarWebCamToolStripMenuItem.Click += new System.EventHandler(this.iniciarWebCamToolStripMenuItem_Click);
+            // 
+            // pararWebCamToolStripMenuItem
+            // 
+            this.pararWebCamToolStripMenuItem.Name = "pararWebCamToolStripMenuItem";
+            this.pararWebCamToolStripMenuItem.Size = new System.Drawing.Size(120, 24);
+            this.pararWebCamToolStripMenuItem.Text = "Parar WebCam";
+            this.pararWebCamToolStripMenuItem.Click += new System.EventHandler(this.pararWebCamToolStripMenuItem_Click);
+            // 
+            // FrmDeteccaoRosto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmDeteccaoRosto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Detecção de Rosto";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem abriImagemToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarWebCamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pararWebCamToolStripMenuItem;
+    }
+}
+
---
+++ ProgramaDeteccaoDeRosto/FrmDeteccaoRosto.cs
@@ -0,0 +1,113 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProgramaDeteccaoDeRosto
+{
+    public partial class FrmDeteccaoRosto : Form
+    {
+        // criando um construtor para pegar o arquivo de treinamento
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        //pegando um vídeo de capture
+        VideoCapture capture;
+
+        public FrmDeteccaoRosto()
+        {
+            InitializeComponent();
+        }
+
+        private void abriImagemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, Filter = "JPEG|*.jpg"})
+            {
+                if(ofd.ShowDialog()== DialogResult.OK)
+                {
+                   // picImagem.Image = Image.FromFile(ofd.FileName);
+                    //Bitmap bitmap = new Bitmap(Image.FromFile(ofd.FileName));
+                    //convertendo a imagem para uma rede de bites
+                    ProcessarImagem(new Bitmap(Image.FromFile(ofd.FileName)));
+                    
+
+                }
+            }
+        }
+
+        private void iniciarWebCamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // fazendo uma verificação na web cam se ela está reiniciada conforme o evento.
+            {
+                if(capture != null)
+                {
+                    MessageBox.Show("A web cam está iniciada!");
+                        return;
+                }
+            }
+
+            //iniciando um class para pegar a imagem, agora para colocar como parâmetro precisa do index da webcam. Como tenho uma webcam vai ser 0 dentro como index.
+            capture = new VideoCapture(0);
+            // o ImageGradded ele executa quando acha um frame da web cam. 
+            capture.ImageGrabbed += Capture_ImageGrabbed;
+            capture.Start();
+        }
+
+        //Esse evendo é chamado cada vez que um evento é capturado da webcam
+        private void Capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat();
+                capture.Retrieve(m);
+                ProcessarImagem(m.Bitmap); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void pararWebCamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(capture == null)
+            {
+                MessageBox.Show("A webCam está desligada");
+                return;
+            }
+            capture.ImageGrabbed -= Capture_ImageGrabbed;
+            capture.Stop();
+            capture.Dispose();
+            capture = null;
+            picImagem.Image = null;
+        }
+            
+        private void ProcessarImagem (Bitmap bitmap)
+        {
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            //vetor de retangulos e esse resultado de preenchimento de retangulo pelo arraycive. O detecteMultiescale é o xml de treinamento. Saber imagem processada e fator de escala.
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+            //por meio dos retangulos encontrar retangulos para achar os arquivos de treinamentos.
+            foreach (var retangle in rectangles)
+            {
+                //o using é um objeto criado para receber os desenhos dos rentângulos
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Blue, 4))
+                    {
+                        graphics.DrawRectangle(pen, retangle);
+                    }
+                }
+                {
+
+                }
+            }
+            picImagem.Image = bitmap;
+
+
+        }
+    
+    
+    }
+
+}
+
